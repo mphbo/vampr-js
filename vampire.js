94b9308e5d5778c4e9c31451a46c495a6c4e9bd6
Logan Thomas <logannormanthomas@protonmail.com>
2021-05-14 05:19:59 +0000

    finished all but the last test on the stretch, that is a hard one

--- vampire.js
+++ vampire.js
@@ -10,22 +10,32 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let count = 0;
+    let vampire = this;
+    while(vampire.creator) {
+      vampire = vampire.creator;
+      count++;
+    }
+    return count;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if(this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+    return false;
   }
 
   /** Stretch **/
@@ -36,7 +46,34 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    if (this.creator === vampire) {
+      return vampire;
+    }
+    if (vampire.creator === this) {
+      return this;
+    }
+    if (!this.creator || !vampire.creator) {
+      return this;
+    }
+    if (this.name === vampire.name) {
+      return vampire;
+    }
+    if (this.numberOfVampiresFromOriginal === vampire.numberOfVampiresFromOriginal) {
+      return this.creator;
+    }
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      // while (vampire.creator) {
+      //   while (this.creator) {
+      //     if (vampire.creator === this.creator) {
+      //       return vampire.creator;
+      //     }
+      //   this = this.creator;
+      //   }
+      //   vampire = vampire.creator;
+      // }
+      return this.creator;
+    }
+    return vampire.creator;
   }
 }
 
